11b664d5bed6ff4503d0638e43090cecf99530d6
Ednardo Gomes <ednardo@gomes.com>
2021-11-17 21:38:22 -0300

    Intalando e configurando o Prettier.

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
   },
 };
