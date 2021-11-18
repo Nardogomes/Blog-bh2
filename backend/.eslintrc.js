d6c91d6755699205a2eaa61ebe7f9209c287552a
Ednardo Gomes <ednardo@gomes.com>
2021-11-17 21:21:26 -0300

    Instalando e configurando o ESLint.

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
